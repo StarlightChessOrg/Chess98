aa1d51dfbdedf24e8f96b376bdd6a5bb71cb6cbb
ForYes <yes-noob@outlook.com>
2026-02-14 15:54:27 +0800

    save

--- Chess98/main.cpp
+++ Chess98/main.cpp
@@ -1,17 +1,6 @@
-﻿// #include "ucci.hpp"
-#include "base.hpp"
+﻿#include "test.hpp"
 
 int main()
 {
-    FEN fen = "rnbakabnr/9/1c5c1/p1p1p1p1p/9/9/P1P1P1P1P/1C5C1/9/RNBAKABNR w";
-    // TEAM team = R;
-    // int max_time = 3000;
-    // int max_depth = 20;
-
-    std::cout << fen::to_fen(fen::to_matrix(fen)) << std::endl;
-
-    // UCCI ucci(fen, team, max_time, max_depth);
-    // ucci.cli();
-
     return 0;
 }
