a4da6d3c7ced0ba6850c3d1f17bd656596d771b2
ForYes <yes-noob@outlook.com>
2026-04-04 22:39:31 +0800

    dev: test

--- src/main.cpp
+++ src/main.cpp
@@ -10,10 +10,10 @@ int main()
         0, 0, 0, 0, 0, 0, 0, 0, 0,
         0, 0, 0, 0, 0, 0, 0, 0, 0,
         0, 0, 0, 0, 0, 0, 0, 0, 0,
-        0, 0, 0, 0, 0, 0, 0, 0, 0,
-        B_KNIGHT, 0, R_CANNON, 0, 0, 0, 0, R_CANNON, 0,
-        0, 0, 0, 0, 0, 0, 0, 0, 0,
-        0, B_PAWN, 0, 0, R_KING, 0, 0, 0, 0
+        0, 0, R_CANNON, 0, 0, 0, 0, 0, 0,
+        0, R_ROOK, R_CANNON, R_KNIGHT, 0, 0, 0, 0, 0,
+        0, 0, B_ROOK, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, R_KING, 0, 0, 0, 0
     };
     position_init(test_matrix, R);
     // test code
@@ -25,12 +25,12 @@ int main()
     // preformance test
     Timer t1 {};
     for (int i = 0; i < 100000000; i++) {
-        gen_cannon_legacy_<true>(R_ROOK_POS);
+        gen_rook_legacy_<true>(R_ROOK_POS);
     }
     std::cout << t1.duration() << "ms\n";
     Timer t2 {};
     for (int i = 0; i < 100000000; i++) {
-        gen_cannon_bit_<true>(R_ROOK_POS);
+        gen_rook_bit_<true>(R_ROOK_POS);
     }
     std::cout << t2.duration() << "ms\n";
     return 0;
