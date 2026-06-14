5c3a267ed7929b5901165446ade86dfc0e223754
ForYes <yes-noob@outlook.com>
2026-06-14 22:28:46 +0800

    dev: fix

--- src/search.hpp
+++ src/search.hpp
@@ -6,18 +6,6 @@
 STATE g_searchstop = 0;
 DEPTH g_maxdepth = 20;
 
-SEARCH_RET search()
-{
-    const Timer timer { 1000 };
-    VL vl { -INF };
-    for (DEPTH depth = 0; !timer.time_up_3xlesser(); depth++) {
-        if (depth > g_maxdepth || (g_searchstop ? g_searchstop-- : 0)) break;
-        vl = search_vl_(depth, -INF, INF);
-    }
-    const Move move = tt_get_move(g_hashkey);
-    return { move, vl };
-}
-
 VL search_vl_(DEPTH depth, VL a, VL b)
 {
     VL vl { -INF };
@@ -28,4 +16,16 @@ VL search_q_(VL b)
 {
     VL vl { -INF };
     return vl;
+}
+
+SEARCH_RET search()
+{
+    const Timer timer { 1000 };
+    VL vl { -INF };
+    for (DEPTH depth = 0; !timer.time_up_3xlesser(); depth++) {
+        if (depth > g_maxdepth || (g_searchstop ? g_searchstop-- : 0)) break;
+        vl = search_vl_(depth, -INF, INF);
+    }
+    const Move move = tt_get_move(g_hashkey);
+    return { move, vl };
 }
