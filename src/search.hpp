1fe0b4e122c5a34929a5f61c73c9995cc52005c2
ForYes <yes-noob@outlook.com>
2026-06-18 19:39:48 +0800

    dev: remove global variables

--- src/search.hpp
+++ src/search.hpp
@@ -2,10 +2,6 @@
 #include "evaluate.hpp"
 #include "moves.hpp"
 
-// global variables
-STATE g_searchstop = 0;
-DEPTH g_maxdepth = 20;
-
 VL search_vl_(DEPTH depth, VL a, VL b)
 {
     VL vl { -INF };
@@ -23,7 +19,6 @@ SEARCH_RET search()
     const Timer timer { 1000 };
     VL vl { -INF };
     for (DEPTH depth = 0; !timer.time_up_3xlesser(); depth++) {
-        if (depth > g_maxdepth || (g_searchstop ? g_searchstop-- : 0)) break;
         vl = search_vl_(depth, -INF, INF);
     }
     const Move move = tt_get_move(g_hashkey);
