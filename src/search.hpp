d61bcfb4a207ee69f3318aa8c5870c80684b6750
ForYes <yes-noob@outlook.com>
2026-06-20 10:46:34 +0800

    dev: defines

--- src/search.hpp
+++ src/search.hpp
@@ -5,6 +5,10 @@
 // const defines
 constexpr DEPTH Q_MAX_DISTANCE { 64 };
 constexpr DEPTH Q_CHECKING_DEPTH { 8 };
+VL search_q_(VL a, VL b, DEPTH depth);
+template <bool CUT>
+VL search_vl_(DEPTH depth, VL a, VL b);
+SEARCH_RET search();
 
 // global variables
 STATE g_searchstop { 0 };
@@ -14,39 +18,16 @@ DEPTH g_maxdepth = { 20 };
 DEPTH distance_ { 0 };
 
 // search
-VL search_q_(VL a, VL b, DEPTH depth)
+SEARCH_RET search()
 {
-    if (distance_ == Q_MAX_DISTANCE || depth == 0) return evaluate();
-    // mdp todo
-    const bool checking = in_check();
-    if (checking) {
-        history_checkings.emplace_back(true);
-        depth = std::min(depth, Q_CHECKING_DEPTH);
-    } else {
-        // ndp todo
+    const Timer timer { 1000 };
+    VL vl { -INF };
+    for (DEPTH depth = 0; !timer.time_up_3xless(); depth++) {
+        if (depth > g_maxdepth || (g_searchstop ? g_searchstop-- : 0)) break;
+        vl = search_vl_<false>(depth, -INF, INF);
     }
-    // repeat status validation todo
-    // search
-    VL vlbest { -INF };
-    std::vector<Move> moves { };
-    if (checking) {
-        moves = gen_all_quiet_moves();
-        history_sort(moves, g_team);
-    } else {
-        moves = gen_all_capture_moves();
-        mvvlva_sort(moves);
-    }
-    for (const Move m : moves) {
-        position_move(m), distance_++;
-        const VL vl = -search_q_(-b, -a, depth - 1);
-        position_undo(), distance_--, history_checkings.pop_back();
-        if (vl > vlbest) {
-            if (vl > b) return vl;
-            vlbest = vl;
-            a = std::max(a, vl);
-        }
-    }
-    return vlbest != -INF ? vlbest : vlbest + distance_;
+    const Move move = tt_get_move(g_hashkey);
+    return { move, vl };
 }
 
 template <bool CUT>
@@ -99,14 +80,37 @@ VL search_vl_(DEPTH depth, VL a, VL b)
     return vlbest != -INF ? vlbest : vlbest + distance_;
 }
 
-SEARCH_RET search()
+VL search_q_(VL a, VL b, DEPTH depth)
 {
-    const Timer timer { 1000 };
-    VL vl { -INF };
-    for (DEPTH depth = 0; !timer.time_up_3xless(); depth++) {
-        if (depth > g_maxdepth || (g_searchstop ? g_searchstop-- : 0)) break;
-        vl = search_vl_<false>(depth, -INF, INF);
+    if (distance_ == Q_MAX_DISTANCE || depth == 0) return evaluate();
+    // mdp todo
+    const bool checking = in_check();
+    if (checking) {
+        history_checkings.emplace_back(true);
+        depth = std::min(depth, Q_CHECKING_DEPTH);
+    } else {
+        // ndp todo
     }
-    const Move move = tt_get_move(g_hashkey);
-    return { move, vl };
+    // repeat status validation todo
+    // search
+    VL vlbest { -INF };
+    std::vector<Move> moves { };
+    if (checking) {
+        moves = gen_all_quiet_moves();
+        history_sort(moves, g_team);
+    } else {
+        moves = gen_all_capture_moves();
+        mvvlva_sort(moves);
+    }
+    for (const Move m : moves) {
+        position_move(m), distance_++;
+        const VL vl = -search_q_(-b, -a, depth - 1);
+        position_undo(), distance_--, history_checkings.pop_back();
+        if (vl > vlbest) {
+            if (vl > b) return vl;
+            vlbest = vl;
+            a = std::max(a, vl);
+        }
+    }
+    return vlbest != -INF ? vlbest : vlbest + distance_;
 }
