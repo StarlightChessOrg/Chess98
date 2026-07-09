fe7edf731eec40b5f812d71d8603587b25e222e3
ForYes <yes-noob@outlook.com>
2026-07-09 13:42:54 +0800

    dev: did some doc works

--- tools/xiangqi_test.py
+++
@@ -1,6 +0,0 @@
-# An application written for testing this project in xiangqi.com
-# Which depends on python selenium and Microsoft Edge on Windows
-# Features:
-# 1. Play against robots
-# 2. Challenge real person via xiangqi.com
-# 3. Auto-continue is defaultly enabled
